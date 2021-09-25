dcf6a90a767d1d6d7c30a02018bc639d61fa327b
zevonjunior <casillas.adrian@googlemail.com>
2021-09-25 21:23:07 +0100

    adding .env file with shopify access token etc and modified index js page title

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home({ products }) {
   return (
     <>
       <Head>
-        <title>Cheese and Meat Shop</title>
+        <title>And Or Cheese and Meat Shop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
